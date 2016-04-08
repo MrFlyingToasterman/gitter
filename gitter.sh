dd057388308e7c15a2e01c3b14a7685536150d8e
MrFlyingToasterman <MrFlyingToasterman@yahoo.de>
2016-04-08 22:19:44 +0200

    gitter 1.0

---
+++ gitter.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+echo "Welcome 2 Gitter 2k16" 
+read -p 'Git username: ' s1
+read -p 'Git Repo: ' s2
+git clone https://www.github.com/$s1/$s2
+read -p 'Git E-mail: ' s3
+read -p 'Commit msg: ' s4
+
+cd $s2
+git config --global user.name "$s1"
+git config --global user.email "$s3"
+git remote -v
+echo "now add all the new files!"
+read -p "Press [Enter]"
+git add *
+git status -s
+git commit -m "$s4"
+echo "uploading..."
+git push origin master
+echo "done!"
+
